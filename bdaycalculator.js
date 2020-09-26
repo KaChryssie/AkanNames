01eb50065a72f57d6396581667b84dc93a8a5398
KaChryssie <karerassie@gmail.com>
2020-09-26 18:54:24 +0200

    add the gender function

--- bdaycalculator.js
+++ bdaycalculator.js
@@ -2,4 +2,13 @@ function calculate() {
     var date = document.getElementById("date").value;
     var month = document.getElementById("month").value;
     var year = document.getElementById("year").value;
+    var gender = null;
+    if(document.getElementById("female").checked){
+        gender = "Female"
     }
+    else if(document.getElementById("male").checked){
+        gender = "male"
+    }
+    
+    
+}
