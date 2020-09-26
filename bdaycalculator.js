228eaa744b2f226ca2e482f24ec60e8ca0983b3d
KaChryssie <karerassie@gmail.com>
2020-09-26 23:46:44 +0200

    add the conditional statement where the function will be called

--- bdaycalculator.js
+++ bdaycalculator.js
@@ -24,4 +24,10 @@ function calculate() {
     var femaleNames = ['Akosua','Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
     var maleNames = ['Kwasi','Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
     var dayOfTheWeek =['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    if(gender === "Female"){
+        alert("Your name is: " + femaleNames[d] + "and you were born on " + dayOfTheWeek[d] ) 
+    }
+    else if(gender === "Male"){
+        alert("Your name is: " + maleNames[d] + "and you were born on " + dayOfTheWeek[d] )
+    }
 }
